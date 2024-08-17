2c512fc48940b3a4ba5bf521109b7a2b6bf050fd
Shadow <aidanlavoie29@gmail.com>
2024-08-17 10:36:15 -0400

    Added the bullet script that was missed in the last commit as well as the melee tower detection

---
+++ GMTK_2024_Game/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b67774984ba1c70c80f989a42fb914f59a28eeb8f401d1f209d6e86e6e532ec0
+size 522
---
+++ GMTK_2024_Game/Assets/Scripts/MeleeTowerManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:611a362f3597e6fb2234f1cb42e449c82af85c5f3d4aa992e3775e0fcd71b25c
+size 1480
